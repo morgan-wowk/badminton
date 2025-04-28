c351b29f57826cae14d89ba13b0fa98e2d8dd43b
Morgan Wowk <morgan.wowk@gmail.com>
2025-04-28 00:37:57 -0700

    Add user instructions
    **Changes:**
    
    * Adds description to offramp and onramp boxes

--- src/pages/SocialMixerPage.tsx
+++ src/pages/SocialMixerPage.tsx
@@ -2,36 +2,56 @@ import { useState } from 'react';
 import Styles from './SocialMixerPage.module.css';
 
 function SocialMixerPage() {
-    const [courts, setCourts] = useState([
+    const [courts,] = useState([
         {}, {}, {}, {}, {}, {},
     ])
-    const [offrampGames, setOfframpGames] = useState((new Array(courts.length)).fill(null));
-    const [onrampGames, setOnrampGames] = useState((new Array(courts.length)).fill(null))
+    const [offrampGames,] = useState((new Array(courts.length)).fill(null));
+    const [onrampGames,] = useState((new Array(courts.length)).fill(null))
+
+    // tslint:disable-next-line
+    const renderOfframp = (game: any, index: Number) => {
+        console.log(game)
+        let className = `${Styles.OfframpGame} ${Styles.OfframpGameEmpty}`;
+        if (index === 0) {
+            className += ` ${Styles.OfframpGameInstruction}`;
+        }
+
+        return (
+            <div className={className} />
+        )
+    }
+
+    // tslint:disable-next-line
+    const renderOnramp = (game: any, index: Number) => {
+        console.log(game)
+        let className = Styles.OnrampGame
+        if (index === 0) {
+            className += ` ${Styles.OnrampGameInstruction}`;
+        }
+
+        return (
+            <div className={className}>
+                <i className={Styles.ArrowUp} />
+            </div>
+        )
+    }
 
     // TODO: Use name Bartholomew D.
 
     return (
         <div className={Styles.Layout}>
             <div className={Styles.GameLayout}>
-                {offrampGames.map((og, i) => (
-                    <div className={Styles.OfframpGame}>{`Offramp ${i}`}</div>
-                ))}
-                {courts.map((c, i) => (
+                {offrampGames.map(renderOfframp)}
+                {courts.map(() => (
                     <div className={Styles.Court}>
                         <i className={Styles.ArrowUp} />
-                        {`Court ${i}`}
                     </div>
                 ))}
-                {onrampGames.map((og, i) => (
-                    <div className={Styles.OnrampGame}>
-                        <i className={Styles.ArrowUp} />
-                        {`Onramp ${i}`}
-                    </div>
-                ))}
+                {onrampGames.map(renderOnramp)}
             </div>
             <div className={Styles.ControlArea}>
                 <div className={Styles.ControlPanel}>
-
+                    Badminton Social Mixer
                 </div>
             </div>
         </div>
